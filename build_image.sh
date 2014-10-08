ab369ab60dcffa610fd4dfc3507d7ec2b1d9b732
Alistair Grant <alistair.grant@ed.ac.uk>
2014-10-08 14:47:32 +0100

    Added Files for build image and removed additional installation on image for testing.

---
+++ build_image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+service=$JOB_NAME
+service_port=$JOB_PORT
+BRANCH=$(git branch | grep '^*' |awk '{print $2}') 
+echo $BRANCH
+
+echo $service:$BRANCH $WORKSPACE
+docker build -t $service:$BRANCH $WORKSPACE
+
+container_id=$(docker run -d -p $service_port $service:$BRANCH)
+container_port=$(docker inspect $container_id | awk 'BEGIN { FS = "\"" } ; /"'$service_port'":/ { print $4 }')
+
+echo "App running on http://localhost:$container_port"
+
+docker stop $container_id
+
+docker run -t trial:master /bin/echo "Hello"
